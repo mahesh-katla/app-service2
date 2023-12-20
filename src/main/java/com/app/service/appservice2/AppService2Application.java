68c2c311f1700e729490646164131f8eff376698
mahesh-katla <katlamaheshkumar@gmail.com>
2023-12-20 22:11:49 +0530

    test

--- src/main/java/com/app/service/appservice2/AppService2Application.java
+++ src/main/java/com/app/service/appservice2/AppService2Application.java
@@ -2,8 +2,7 @@ package com.app.service.appservice2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
 
 @SpringBootApplication
 //@EnableDiscoveryClient
